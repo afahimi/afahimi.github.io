fa38e17eac0679191e1c23dd5ebf3d4d30116a9b
afahimi <amin.f1000@gmail.com>
2023-07-08 12:25:40 -0700

    added modularity

--- src/components/pages/Projects/Projects.js
+++ src/components/pages/Projects/Projects.js
@@ -7,53 +7,53 @@ import TextContainer from "../../UI/Elements/TextContainer/TextContainer";
 import ProjectItem from "./Components/ProjectItem";
 
 const Projects = () => {
-  const backgroundPicker = (index) => {
-    if (index % 2 === 0) {
-      return "#B2D4E1";
-    } else {
-      return "#CCE0E5";
+  const projects = [
+    {
+      date: "January 31st, 2099",
+      people: "Amin, Ardi, Mercury",
+      img: "/images/ProjectImages/test.jpg",
+      title: "Sample Project One",
+      description: `Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+      eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+      enim ad minim veniam, quis nostrud exercitation ullamco laboris
+      nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in
+      reprehenderit in voluptate velit esse cillum dolore eu fugiat
+      nulla pariatur. Excepteur sint occaecat cupidatat non proident,
+      sunt in culpa qui officia deserunt mollit anim id est laborum.`,
+      location: "1234 Main Street, City, State, 12345",
+      keywords: ["React", "Embedded Systems", "C++"],
+      href: "https://github.com/username"
+    },
+    {
+      date: "August 25th, 2003",
+      people: "Amin",
+      img: "/images/ProjectImages/test2.jpg",
+      title: "Sample Project Two",
+      description: `Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+      eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+      enim ad minim veniam, quis nostrud exercitation ullamco laboris
+      nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in
+      reprehenderit in voluptate velit esse cillum dolore eu fugiat
+      nulla pariatur. Excepteur sint occaecat cupidatat non proident,
+      sunt in culpa qui officia deserunt mollit anim id est laborum.`,
+      location: "Somewhere in Vancouver, BC",
+      keywords: ["CI/CD", "Physical Design", "C++"],
+      href: "https://github.com/username"
     }
-  }
+  ];
+  
   return (
     <div className={classes.titleScreen}>
       <H1>Projects</H1>
       <br />
-      <ProjectItem
-      right={true}
-        date="January 31st, 2099"
-        people="Amin, Ardi, Mercury"
-        img="/images/ProjectImages/test.jpg"
-        backgroundColor="#B2D4E1"
-        title="Sample Project One"
-        description=" Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-              eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
-              enim ad minim veniam, quis nostrud exercitation ullamco laboris
-              nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in
-              reprehenderit in voluptate velit esse cillum dolore eu fugiat
-              nulla pariatur. Excepteur sint occaecat cupidatat non proident,
-              sunt in culpa qui officia deserunt mollit anim id est laborum."
-        location="1234 Main Street, City, State, 12345"
-        keywords={["React", "Embedded Systems", "API's"]}
-        href="https://github.com/username"
-      />
-      <ProjectItem
-      right={false}
-        date="January 31st, 2099"
-        people="Amin, Ardi, Mercury"
-        img="/images/ProjectImages/test.jpg"
-        backgroundColor="#CCE0E5"
-        title="Sample Project One"
-        description=" Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-              eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
-              enim ad minim veniam, quis nostrud exercitation ullamco laboris
-              nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in
-              reprehenderit in voluptate velit esse cillum dolore eu fugiat
-              nulla pariatur. Excepteur sint occaecat cupidatat non proident,
-              sunt in culpa qui officia deserunt mollit anim id est laborum."
-        location="1234 Main Street, City, State, 12345"
-        keywords={["React", "Embedded Systems", "API's"]}
-        href="https://github.com/username"
-      />
+      {projects.map((project, index) => (
+        <ProjectItem
+          key={index}
+          right={index % 2 === 0 ? true : false}
+          backgroundColor={index % 2 === 0 ? "#B2D4E1" : "#CCE0E5"}
+          {...project}
+        />
+      ))}
     </div>
   );
 };
