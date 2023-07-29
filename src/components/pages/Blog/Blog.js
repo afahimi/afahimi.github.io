a7afac542e0f0105082df54325356a703f710bfc
afahimi <amin.f1000@gmail.com>
2023-07-29 12:48:43 -0700

    ready for initial release

--- src/components/pages/Blog/Blog.js
+++ src/components/pages/Blog/Blog.js
@@ -1,14 +1,15 @@
 import React from "react";
-import {H1} from "../../UI/Elements/Typography/Typography";
+import { H1 } from "../../UI/Elements/Typography/Typography";
+import classes from "./Blog.module.css"
 
 const Blog = () => {
-    return (
-        <>
-            <H1>
-                Blog
-            </H1>
-        </>
-    )
-}
+  return (
+    <>
+      <div className={classes.blog}>
+        <H1>Blog: Work in progress... 🚧</H1>
+      </div>
+    </>
+  );
+};
 
 export default Blog;
--- src/components/pages/Notes/Notes.js
+++ src/components/pages/Notes/Notes.js
@@ -1,14 +1,15 @@
 import React from "react";
-import {H1} from "../../UI/Elements/Typography/Typography";
+import { H1 } from "../../UI/Elements/Typography/Typography";
+import classes from "./Notes.module.css";
 
 const Notes = () => {
-    return (
-        <>
-            <H1>
-                Notes
-            </H1>
-        </>
-    )
-}
+  return (
+    <>
+      <div className={classes.notes}>
+        <H1>Notes: Work in progress... 🚧</H1>
+      </div>
+    </>
+  );
+};
 
 export default Notes;
