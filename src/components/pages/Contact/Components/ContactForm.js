f074bb5af7db510160e931b2d3acb90502a312a4
afahimi <amin.f1000@gmail.com>
2023-07-29 12:36:23 -0700

    added email functionality

--- src/components/pages/Contact/Components/ContactForm.js
+++ src/components/pages/Contact/Components/ContactForm.js
@@ -1,49 +1,80 @@
-import React, { useState } from 'react';
-import axios from 'axios';
-import classes from './ContactForm.module.css';
+import React, { useState } from "react";
+import classes from "./ContactForm.module.css";
+import emailjs from '@emailjs/browser';
 
 const ContactForm = () => {
-    const [email, setEmail] = useState("");
-    const [subject, setSubject] = useState("");
-    const [message, setMessage] = useState("");
+  const [email, setEmail] = useState("");
+  const [subject, setSubject] = useState("");
+  const [message, setMessage] = useState("");
 
-    const handleSubmit = (e) => {
-        e.preventDefault();
+  const form = React.useRef();
 
-        axios({
-            method: "POST",
-            url:"http://localhost:3002/send", //TODO change this to the actual url
-            data: {
-                email: email,
-                subject: subject,
-                message: message
-            }
-        }).then((response)=>{
-            if (response.data.status === 'success'){
-                alert("Message Sent.");
-                resetForm()
-            } else if(response.data.status === 'fail'){
-                alert("Message failed to send.")
-            }
-        })
-    }
+  const sendEmail = (e) => {
+    e.preventDefault();
 
-    const resetForm=()=>{
-        setEmail("");
-        setSubject("");
-        setMessage("");
-    }
+    emailjs
+      .sendForm(
+        "service_0bgpeia",
+        "template_ocnsui8",
+        form.current,
+        "dJCK86fw67UJAlXpv"
+      )
+      .then(
+        (result) => {
+          console.log(result.text);
+          alert("Email sent successfully!");
+          resetForm();
+        },
+        (error) => {
+          console.log(error.text);
+          alert("Failed to send email, please try again.");
+        }
+      );
+  };
 
-    return (
-        <div className={classes.contactForm}>
-            <form onSubmit={handleSubmit} method="POST">
-                <input type="email" className={classes.inputField} placeholder="Your email" value={email} onChange={(e) => setEmail(e.target.value)} required/>
-                <input type="text" className={classes.inputField} placeholder="Subject" value={subject} onChange={(e) => setSubject(e.target.value)} required/>
-                <textarea className={classes.textArea} placeholder="Message" value={message} onChange={(e) => setMessage(e.target.value)} required/>
-                <input type="submit" className={classes.submitButton} value="Send Message"/>
-            </form>
-        </div>
-    );
-}
+  const resetForm = () => {
+    setEmail("");
+    setSubject("");
+    setMessage("");
+  };
+
+  return (
+    <div className={classes.contactForm}>
+      <form ref={form} onSubmit={sendEmail} method="POST">
+        <input
+          type="email"
+          className={classes.inputField}
+          placeholder="Your email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+          name="user_email"
+          required
+        />
+        <input
+          type="text"
+          className={classes.inputField}
+          placeholder="Subject"
+          value={subject}
+          onChange={(e) => setSubject(e.target.value)}
+          name="user_name"
+          required
+        />
+        <textarea
+          className={classes.textArea}
+          placeholder="Message"
+          value={message}
+          onChange={(e) => setMessage(e.target.value)}
+          name="message"
+          required
+        />
+        <input
+          type="submit"
+          className={classes.submitButton}
+          value="Send Message"
+        />
+      </form>
+    </div>
+  );
+};
 
 export default ContactForm;
